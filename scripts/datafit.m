a77be2becb5fec7aa9aa1cfad2f4e501808bfa21
giTimo <t.bretten@gmail.com>
2012-10-17 17:13:46 +0200

    refined fit, flashes in suns

--- scripts/datafit.m
+++ scripts/datafit.m
@@ -2,14 +2,13 @@ clear all; clf;
  
 # Function that will be fit
 function [y]=double_exp(x,par)
-  y=par(1)*exp(-par(2)*x)-par(3)*exp(-par(4)*x);
+  y=par(1)*exp(-par(2)*x)-par(1)*exp(-par(3)*x);
 end
  
 # Generate a double exponent
-J0b=0.02;
-t1=1/0.0001;
-J0p=0.02;
-t2=1/0.005;
+j=1.4198e-02;
+t1= 2.6032e+02;
+t2= 4.2686e+02;
 m=load("../data/2012-10-16/flash_middle_light_off_cut");
 y=m(:,2);
 x=m(:,1);
@@ -20,11 +19,10 @@ ymin=1.2*min(y);
 ymax=1.2*max(y);
 # Add some noise to the line
 
-weights=ones(size(x));
-
+weights=ones(size(y));%.*abs((y/0.0008));
  
 # Perform the fit
-pin=[J0b,t1,J0p,t2];
+pin=[j,t1,t2];
 [f,p,cvg,iter,corp,covp]=leasqr(x,y,pin,"double_exp",.0001,20,weights);
 
 diff=double_exp(x,p)-y;
@@ -32,7 +30,7 @@ diff=double_exp(x,p)-y;
 # Print out the results
 p
 covp
-sum((y-double_exp(x,p)).^2.*weights.^2)
+sum(abs((y-double_exp(x,p))))
  
 # Plots
 subplot(2,1,1)
@@ -44,7 +42,7 @@ title "Data and fit";
 axis([xmin,xmax,ymin,ymax]);
  
 subplot(2,1,2);
-plot(m(:,1),diff);
+plot(m(:,1),diff(:));
 title "Fitted values minus measured values";
 axis([xmin,xmax,1.2*min(diff),1.2*max(diff)]);
 hold off;
