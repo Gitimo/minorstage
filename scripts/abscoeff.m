41d4b339945ed4de2b942ad15a688ffb04e3a2b4
giTimo <t.bretten@gmail.com>
2013-05-24 00:14:48 +0200

    looking into IQE analysis

---
+++ scripts/abscoeff.m
@@ -0,0 +1,18 @@
+function [y]=abscoeff(E,Eg)
+format long;
+	q=1.60217657e-19;	#elementary charge
+	a_0=800000;		#absorption-coefficient for GaAs 8000/cm -> 8000 00/m
+	E_0=q*6.7e-3;		#Urbach energy: 6.7 meV
+	E_d=q*140e-3;		#
+	
+	g(1:max(size(E)))=0;
+
+	for m=1:max(size(E))
+		if E(m)<=Eg
+			g(m)=a_0*exp((E(m)-Eg)/E_0);
+		else
+			g(m)=a_0*(1+(E(m)-Eg)/E_d);
+		endif
+	endfor
+	y=g;
+end
