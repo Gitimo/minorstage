ad3954d3432cffc64e571f68caee313193dff4a4
giTimo <t.bretten@gmail.com>
2013-05-30 17:35:18 +0200

    IQE analysis (half) done; EQE null test done

--- scripts/abscoeff.m
+++ scripts/abscoeff.m
@@ -1,10 +1,10 @@
 function [y]=abscoeff(E,Eg)
 format long;
 	q=1.60217657e-19;	#elementary charge
-	a_0=800000;		#absorption-coefficient for GaAs 8000/cm -> 8000 00/m
+	a_0=800;		#absorption-coefficient for GaAs 8000/cm -> 800/mm
 	E_0=q*6.7e-3;		#Urbach energy: 6.7 meV
 	E_d=q*140e-3;		#
-	
+	Eg=Eg*q;
 	g(1:max(size(E)))=0;
 
 	for m=1:max(size(E))
